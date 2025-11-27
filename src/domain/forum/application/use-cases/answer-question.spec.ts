9cf759cb7125b52eaa61e13753160e9330b33fbf
Pedro Souza <pedro.olegario23@gmail.com>
2025-11-27 16:08:55 -0300

    ✨feat: notifications  delivery

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,7 +1,7 @@
-import { UniqueEntityID } from '@/core/entities/unique-entity-id'
-import { AnswerQuestionUseCase } from '@/domain/forum/application/use-cases/answer-question'
-import { InMemoryAnswerAttachmentsRepository } from 'test/repositories/in-memory-answer-attachments-repository'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { AnswerQuestionUseCase } from './answer-question'
 import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { InMemoryAnswerAttachmentsRepository } from 'test/repositories/in-memory-answer-attachments-repository'
 
 let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository
 let inMemoryAnswersRepository: InMemoryAnswersRepository
@@ -14,6 +14,7 @@ describe('Create Answer', () => {
     inMemoryAnswersRepository = new InMemoryAnswersRepository(
       inMemoryAnswerAttachmentsRepository,
     )
+
     sut = new AnswerQuestionUseCase(inMemoryAnswersRepository)
   })
 
@@ -26,14 +27,18 @@ describe('Create Answer', () => {
     })
 
     expect(result.isRight()).toBe(true)
-    expect(inMemoryAnswersRepository.items[0]).toEqual(result.value?.answer)
+    expect(inMemoryAnswersRepository.items).toHaveLength(1)
+    expect(inMemoryAnswersRepository.items[0].id).toEqual(
+      result.value?.answer.id,
+    )
+
     expect(
       inMemoryAnswersRepository.items[0].attachments.currentItems,
     ).toHaveLength(2)
     expect(inMemoryAnswersRepository.items[0].attachments.currentItems).toEqual(
       [
-        expect.objectContaining({ attachmentId: new UniqueEntityID('1') }),
-        expect.objectContaining({ attachmentId: new UniqueEntityID('2') }),
+        expect.objectContaining({ attachmentId: new UniqueEntityId('1') }),
+        expect.objectContaining({ attachmentId: new UniqueEntityId('2') }),
       ],
     )
   })
