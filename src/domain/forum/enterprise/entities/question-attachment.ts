88494b13197da78d37ff61852c1de98945b2c544
Pedro Souza <pedro.olegario23@gmail.com>
2025-11-27 00:58:24 -0300

    ✨feat: add edit question test

--- src/domain/forum/enterprise/entities/question-attachment.ts
+++ src/domain/forum/enterprise/entities/question-attachment.ts
@@ -1,13 +1,13 @@
 import { Entity } from '@/core/entities/entity'
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 
-interface QuestionAttachmentProps {
+export interface QuestionAttachmentProps {
   questionId: UniqueEntityId
   attachmentId: UniqueEntityId
 }
 
 export class QuestionAttachment extends Entity<QuestionAttachmentProps> {
-  get topicId() {
+  get questionId() {
     return this.props.questionId
   }
 
@@ -16,8 +16,8 @@ export class QuestionAttachment extends Entity<QuestionAttachmentProps> {
   }
 
   static create(props: QuestionAttachmentProps, id?: UniqueEntityId) {
-    const attachment = new QuestionAttachment(props, id)
+    const questionAttachment = new QuestionAttachment(props, id)
 
-    return attachment
+    return questionAttachment
   }
 }
